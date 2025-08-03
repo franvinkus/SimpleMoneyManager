9148a4100ee5778cd254c111bb5aadd5f011ed27
KielNih <hezkielchris@gmail.com>
2025-08-03 22:42:44 +0700

    feat(homescreen): update UI and add OCR icon
    - Update homescreen UI to include net balance container
    - Add OCR icon to BottomBar component
    - Adjust styles for better visual presentation
    - Update dependencies and add MIT license to async-storage

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -2,13 +2,10 @@ import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { View, Text, StyleSheet, SafeAreaView, ScrollView, TouchableOpacity, Alert, Image} from 'react-native';
 import ButtonBottomBar from './ButtonBottomBar';
-import CatIcon from '../assets/icons/CatIcon.png';
+import OCR_icon from '../assets/icons/OCR-icon.png';
 import Calendar from '../assets/icons/Calendar.png';
 import { useModal } from '../context/modalContext';
 import AddMoneyIcon from '../assets/icons/AddMoneyIcon.png'
-
-
-
 
 const BottomBar = () => {
     const navigation = useNavigation();
@@ -18,14 +15,14 @@ const BottomBar = () => {
         requestAddMoneyModal();
     };
     return (
-        <View style={{height: 75, backgroundColor: "black"}}>
+        <View style={{height: 90, backgroundColor: "black"}}>
             <View style={[styles.container]}>
                 <View style={[styles.buttonRow]}>
                     <ButtonBottomBar screen='Splash' icon={Calendar}/>
-                    <ButtonBottomBar screen='Camera' icon={CatIcon}/>
+                    <ButtonBottomBar screen='Camera' icon={OCR_icon}/>
                     <TouchableOpacity onPress={handleAddMoneyPress}>
-                        {/* Ganti dengan icon atau teks yang relevan */}
-                        <Image source={AddMoneyIcon} style={{width: 80, height: 87, borderRadius: 15, marginLeft: 10}}/>
+                        {/* Ganti dengan icon atau teks yang relevan */}s
+                        <Image source={AddMoneyIcon} style={{width: 75, height: 80, borderRadius: 15, marginLeft: 10}}/>
                     </TouchableOpacity>
                 </View>
             </View>
@@ -36,6 +33,7 @@ const BottomBar = () => {
 
 const styles = StyleSheet.create({
     container: {
+        marginTop:15,
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
